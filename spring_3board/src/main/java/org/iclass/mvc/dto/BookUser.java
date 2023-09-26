15dbe6dc9fde1b87a9d91140c0b1eb5697e62d80
Ijongwon <witmrv1@gmail.com>
2023-09-26 17:39:58 +0900

    dao,dto 추가사항
    dao, dto 추가

--- spring_3board/src/main/java/org/iclass/mvc/dto/BookUser.java
+++ spring_3board/src/main/java/org/iclass/mvc/dto/BookUser.java
@@ -9,19 +9,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @Getter
 @Setter
-@ToString
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class CommunityComments {
-	private int idx;
-	private long mref;
-	private String writer;
-	private String content;
-	private LocalDate createdAt;
-	private String ip;
-	private int heart;
+@NoArgsConstructor
+@ToString
+public class BookUser {	
+	private String id;
+	private String name;
+	private String email;
+	private String age;
+	private String password;
 
 }
